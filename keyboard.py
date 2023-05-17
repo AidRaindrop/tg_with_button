21291ddda4e1170d579023cdbd3e2fd0ce58a54b
eduard <burak.edouard@yandex.by>
2023-05-17 10:53:00 +0300

    addButtonInline

--- keyboard.py
+++ keyboard.py
@@ -36,3 +36,17 @@ markup_request = ReplyKeyboardMarkup(resize_keyboard=True).add(
 ).add(
     KeyboardButton('Отправить свою локацию 🗺️', request_location=True)
 )
+#inline buttons
+inline_btn_1 = InlineKeyboardButton('Первая кнопка!', callback_data='button1')
+inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1)
+inline_kb_full = InlineKeyboardMarkup(row_width=2).add(inline_btn_1)
+inline_kb_full.add(InlineKeyboardButton('Вторая кнопка', callback_data='btn2'))
+inline_btn_3 = InlineKeyboardButton('кнопка 3', callback_data='btn3')
+inline_btn_4 = InlineKeyboardButton('кнопка 4', callback_data='btn4')
+inline_btn_5 = InlineKeyboardButton('кнопка 5', callback_data='btn5')
+inline_kb_full.add(inline_btn_3, inline_btn_4, inline_btn_5)
+inline_kb_full.row(inline_btn_3, inline_btn_4, inline_btn_5)
+inline_kb_full.insert(InlineKeyboardButton("query=''", switch_inline_query=''))
+inline_kb_full.insert(InlineKeyboardButton("query='qwerty'", switch_inline_query='qwerty'))
+inline_kb_full.insert(InlineKeyboardButton("Inline в этом же чате", switch_inline_query_current_chat='wasd'))
+inline_kb_full.add(InlineKeyboardButton('Уроки aiogram', url='https://surik00.gitbooks.io/aiogram-lessons/content/'))
--- main1.py
+++ main1.py
@@ -10,14 +10,22 @@ from config import TOKEN
 from utils import TestStates
 from messages import MESSAGES
 
+import help_mes as hm
 import keyboard as kb
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 dp.middleware.setup(LoggingMiddleware())
 
+@dp.callback_query_handler(lambda c: c.data == 'button1')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Нажата первая кнопка!')
+
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply("Привет!", reply_markup=kb.greet_kb2)
+
+
 
 @dp.message_handler(commands=['hi3'])
 async def process_hi3_command(message: types.Message):
@@ -35,11 +43,33 @@ async def process_hi5_command(message: types.Message):
 async def process_hi6_command(message: types.Message):
     await message.reply("Шестое - запрашиваем контакт и геолокацию\nЭти две кнопки не зависят друг от друга", reply_markup=kb.markup_request)
 
+
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply(MESSAGES['help'])
+    await message.reply(hm.help_message)
 
+@dp.message_handler(commands=['1'])
+async def process_command_1(message: types.Message):
+    await message.reply("Первая инлайн кнопка", reply_markup=kb.inline_kb1)
 
+@dp.callback_query_handler(lambda c: c.data and c.data.startswith('btn'))
+async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
+    code = callback_query.data[-1]
+    if code.isdigit():
+        code = int(code)
+    if code == 2:
+        await bot.answer_callback_query(callback_query.id, text='Нажата вторая кнопка')
+    elif code == 5:
+        await bot.answer_callback_query(
+            callback_query.id,
+            text='Нажата кнопка с номером 5.\nА этот текст может быть длиной до 200 символов 😉', show_alert=True)
+    else:
+        await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, f'Нажата инлайн кнопка! code={code}')
+
+@dp.message_handler(commands=['2'])
+async def process_command_2(message: types.Message):
+    await message.reply("Отправляю все возможные кнопки", reply_markup=kb.inline_kb_full)
 
 @dp.message_handler(state='*', commands=['setstate'])
 async def process_setstate_command(message: types.Message):
@@ -79,6 +109,8 @@ async def some_test_state_case_met(message: types.Message):
 @dp.message_handler()
 async def echo_message(msg: types.Message):
     await bot.send_message(msg.from_user.id, msg.text)
+
+
 
 async def shutdown(dispatcher: Dispatcher):
     await dispatcher.storage.close()
